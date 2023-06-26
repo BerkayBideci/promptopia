f9f1e082af687bcbb4250ec7e66b6fb2673251a9
Berkay Bideci <berkaybideci@gmail.com>
2023-06-26 21:03:48 +0300

    Fix: fix feed error

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -21,12 +21,13 @@ const Feed = () => {
     const [posts, setPosts] = useState([]);
     const handleSearchChange = (e) => {};
 
+    const fetchPosts = async () => {
+        const response = await fetch("/api/prompt");
+        const data = await response.json();
+        setPosts(data);
+    };
+
     useEffect(() => {
-        const fetchPosts = async () => {
-            const response = await fetch("/api/prompt");
-            const data = await response.json();
-            setPosts(data);
-        };
         fetchPosts();
     }, []);
 
